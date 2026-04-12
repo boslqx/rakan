ba217896da5c76a304d6a8b946526d8862226bde
boslqx <bosliangqx@gmail.com>
2026-04-12 15:47:25 +0800

    setup color constant
    background color, mettalic accent, text, and semantic

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  AppColors._();
+
+  // Background 
+  static const Color background = Color(0xFF0C0E10); // Main app background color
+  static const Color surfaceContainerLow = Color(0xFF13161A); // Backgfround for cards, sections
+  static const Color surfaceContainerHigh = Color(0xFF1C2026); // Active cards, modal
+  static const Color surfaceContainerLowest = Color(0xFF000000); // Photo Background
+  static const Color surfaceBright = Color(0xFF252A30); // Inactive chips
+
+  // Metallic Accents
+  static const Color primary = Color(0xFFC6C6C7); // Main accent
+  static const Color primaryContainer = Color(0xFF9B9EA3); // Secondaryy accent
+  static const Color onPrimary = Color(0xFF0C0E10); // Text on primary
+
+  // Text
+  static const Color onSurface = Color(0xFFE8E8E9); // Primary color
+  static const Color onSurfaceVariant = Color(0xFF9B9EA3); // Secondary/Muted text
+
+  // Semantic
+  static const Color error = Color(0xFFEE7D77); // Error color
+  static const Color outlineVariant = Color(0xFF2A2D32); // Ghost borders
+
+
+}
